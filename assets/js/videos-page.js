cafebda684bd76af0e009f3715a169767dfa8281
matthova <matthova87@gmail.com>
2017-02-04 10:11:04 -0800

    initial commit

---
+++ assets/js/videos-page.js
@@ -0,0 +1,32 @@
+angular.module('brushfire_videosPage', [])
+.config(['$sceDelegateProvider', function($sceDelegateProvider) {
+  $sceDelegateProvider.resourceUrlWhitelist([
+    'self',
+    '*://www.youtube.com/**'
+  ]);
+}]);
+
+angular.module('brushfire_videosPage').controller('PageCtrl', [
+            '$scope', '$timeout',
+  function ( $scope ,  $timeout ){
+
+    $scope.videosLoading = true;
+
+    $timeout(function afterRetrievingVideos (){
+      var _videos = [{
+        title: 'FUNNY BABY VIDEOS',
+        src: 'https://www.youtube.com/embed/_FvTVWjLiHM'
+      }, {
+        title: 'Justin Bieber - Baby ft. Ludacris',
+        src: 'https://www.youtube.com/embed/kffacxfA7G4'
+      }, {
+        title: 'Charlie bit my finger - again !',
+        src: 'https://www.youtube.com/embed/_OBlgSz8sSM'
+      }];
+
+      $scope.videosLoading = false;
+      $scope.videos = _videos;
+
+    }, 1000);
+  }
+]);
